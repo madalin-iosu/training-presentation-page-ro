8eabbe028e619374fca0e2f9284b8a8bcfd19dc2
madalin-iosu <user@test.com>
2023-03-15 11:10:59 +0100

    site name

--- src/layouts/Rootlayout.js
+++ src/layouts/Rootlayout.js
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
-import Breadcrumbs from "../components/breadcrumbs/Breadcrumbs";
 
-export default function Rootlayout () {
-    return(
+export default function Rootlayout() {
+    return (
         <div>
             <Navbar />
             < Outlet />
